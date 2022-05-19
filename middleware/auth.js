9b94a7d5e80da0dc1756a11501dbb0b9bd16de68
equarmx <equarmx@gmail.com>
2022-05-19 21:05:15 +0300

    redirection to article

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,3 @@
-import LoginAuth from "../components/frontLayouts/LoginAuth";
-import Logging from "../services/logging";
-import Request from "@/services/request";
-
 export default async function ({ app, res, query, store, route, redirect }) {
-  // RIP
-  // console.log('route.query', route.query)
-  // console.log('app', app);
-  //
-  // if (route.query.userEmail) {
-  //   await store.dispatch('loginUser', {'userEmail': route.query.userEmail});
-  // }
-  //
-  // if (process.env.NODE_ENV === 'production') {
-  //   console.log(app.$cookiz.get('accessToken'))
-  //   if (app.$cookiz.get('accessToken') === null) {
-  //     const refreshResponse = await store.dispatch('refreshTokens')
-  //
-  //     if (Logging.checkExistErr(refreshResponse))
-  //       return redirect('/login')
-  //   }
-  // }
+  return redirect('/articles')
 }
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  middleware: ['auth'],
 }
 </script>
