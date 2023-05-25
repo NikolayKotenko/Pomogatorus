106251081cdbdcd7fd1f54d07677bc2c6a5e893c
Sergey <sr.kotenko.1703@gmail.com>
2023-05-25 22:32:47 +0500

    - Попытки причесать загрузку файлов в Статье(Стили). - Вызов виджета объекта в Хэдере. Правки стилей хэдера.

---
+++ components/Widgets/WrapperStickyCurrentObject.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper_current_object">
+    <CurrentObjects class="current_local_object"></CurrentObjects>
+  </div>
+</template>
+
+<script>
+import CurrentObjects from "./CurrentObjects.vue";
+
+export default {
+  name: "WrapperStickyCurrentObject",
+  components: {CurrentObjects}
+}
+</script>
+
+<style scoped lang="scss">
+.wrapper_current_object {
+  height: 1px;
+  //width: 1px;
+  top: 15px;
+  position: sticky;
+  .current_local_object{
+    top: 0;
+    //right: -320px;
+    position: absolute;
+  }
+}
+</style>
