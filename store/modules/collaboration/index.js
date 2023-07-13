bd7990cca7369c35f4395dc4d1fb31b913795d0c
nikolay-kotenko <nikolay-kotenko@mail.ru>
2023-07-13 16:24:10 +0500

    - логика по запросам из блока коллаборация, отрисовываются пользователи корректно, поправьте карточки списочные

--- store/modules/collaboration/index.js
+++ store/modules/collaboration/index.js
@@ -6,24 +6,40 @@ export default {
     stateBlock: false,
     isLoading: false,
     listMembers: [],
+    listSearchedMembers: [],
   },
   mutations: {
     setLoading(state, payload) {
       state.isLoading = payload
     },
     setMembersList(state, payload) {
+      state.listSearchedMembers = [] //обнуляем поиск искомых юзеров
       state.listMembers = []
       state.listMembers = payload
     },
+    setSearchedMembersList(state, payload) {
+      state.listSearchedMembers = []
+      state.listSearchedMembers = payload
+    },
   },
   actions: {
-    async getListMembers({ commit }, Obj) {
+    async getSearchedListMembers({ commit }, string) {
+      console.log('getSearchedListMembers', string)
+      if (!string) return false
+
+      const response = await Request.get(
+        this.state.BASE_URL + '/users/get-list-users/search?q=' + string
+      )
+      const result = response.data.map((elem) => elem.data) //Преобразуем из поисковой структуры в обычный массив
+
+      commit('setSearchedMembersList', result)
+      commit('setLoading', false)
+      return response
+    },
+    async getListMembersByFilter({ commit }, Obj) {
       commit('setLoading', true)
 
-      const query = Request.ConstructFilterQuery(Obj)
-      // query = ?filter[id_object]=2
-      // query = ?filter[searchPhrase]=sdfsdfsdfs
-
+      const query = Request.ConstructFilterQuery(Obj) // query = ?filter[id_object]=2
       const response = await Request.get(
         this.state.BASE_URL + '/users/get-list-users' + query
       )
